7b523229ce4c62681d916b570f16d3e042abf7c9
shibudaniel <47894492+shibuadaniel@users.noreply.github.com>
2020-02-25 22:45:22 -0500

    Initial Commit

---
+++ PeopleList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PeopleList
+//
+//  Created by SHIBU DANIEL on 2/25/20.
+//  Copyright © 2020 Skie Design Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
