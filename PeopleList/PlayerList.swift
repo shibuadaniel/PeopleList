0eb6c07e4abd8d145e0528adc9f3b254be10110a
shibudaniel <47894492+shibuadaniel@users.noreply.github.com>
2020-02-28 14:52:03 -0500

    PlayerList

---
+++ PeopleList/PlayerList.swift
@@ -0,0 +1,29 @@
+//
+//  PlayerList.swift
+//  PeopleList
+//
+//  Created by SHIBU DANIEL on 2/28/20.
+//  Copyright © 2020 Skie Design Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlayerList: View {
+    var body: some View {
+        NavigationView {
+            List(players) {
+                currentPlayer in
+                NavigationLink(destination: ContentView()) {
+                    PlayerRow(player: currentPlayer)
+                        .frame(height:60)
+                }
+            }.navigationBarTitle(Text("NBA Players"))
+        }
+    }
+}
+
+struct PlayerList_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerList()
+    }
+}
